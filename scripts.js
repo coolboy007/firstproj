9dbae6fa14773cc7d126b5778324ff507caed2ec
coolboy007 <61577388+coolboy007@users.noreply.github.com>
2021-07-07 21:37:16 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,26 @@
-var search_input = document.querySelector("#search_input");
-
-search_input.addEventListener("keyup", function(e){
-  var span_items = document.querySelectorAll(".table_body .name span");
-  var table_body = document.querySelector(".table_body ul");
-  var search_item = e.target.value.toLowerCase();
- 
- span_items.forEach(function(item){
-   if(item.textContent.toLowerCase().indexOf(search_item) != -1){
-      item.closest("li").style.display = "block";
-   }
-   else{
-     item.closest("li").style.display = "none";
-     }
- })
-  
-});
+ <script>  
+      $(document).ready(function(){  
+           $('#search').keyup(function(){  
+                search_table($(this).val());  
+           });  
+           function search_table(value){  
+                $('#employee_table tr').each(function(){  
+                     var found = 'false';  
+                     $(this).each(function(){  
+                          if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0)  
+                          {  
+                               found = 'true';  
+                          }  
+                     });  
+                     if(found == 'true')  
+                     {  
+                          $(this).show();  
+                     }  
+                     else  
+                     {  
+                          $(this).hide();  
+                     }  
+                });  
+           }  
+      });  
+ </script>
